7a73fe799a0ae31af9aa03f60afa936cf268fb18
jim <>
2020-11-27 23:21:14 +0800

    主线程调用toast

--- SightKit/Classes/Toast+Loading.swift
+++ SightKit/Classes/Toast+Loading.swift
@@ -20,36 +20,38 @@ public func showToast(toast:String?,fromView: UIView?=nil,duration:TimeInterval=
         return
     }
     
-    //防止重复添加
-    if let existToastView = fromView.viewWithTag(tagForToastView) {
-        existToastView.removeFromSuperview()
-    }
-    
-    let containView = UIView()
-    containView.tag = tagForToastView
-    fromView.addSubview(containView)
-    containView.csFullfill()
-    
-    let shadowBg = UIView()
-    containView.addSubview(shadowBg)
-    shadowBg.backgroundColor = .black
-    shadowBg.alpha = 0.7
-    shadowBg.corner(radius: 5)
-    shadowBg.csCenter()
-    NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "H:|->=20-[v1]->=20-|", options: [], metrics: nil, views: ["v1":shadowBg]))
-    
-    let toastLabel = UILabel().wTextColor(.white).wFont(pfr(18)).wText(toast)
-    shadowBg.addSubview(toastLabel)
-    toastLabel.numberOfLines = 0
-    toastLabel.lineBreakMode = .byCharWrapping
-    toastLabel.csFullfill(top: 10, left: 10, bottom: -10, right: -10)
-    
-    let walltime = DispatchWallTime.now() + duration
-    DispatchQueue.main.asyncAfter(wallDeadline: walltime) {
-        UIView.animate(withDuration: 0.5, animations: {
-            containView.alpha = 0
-        }) { (finish) in
-            containView.removeFromSuperview()
+    DispatchQueue.main.async {
+        //防止重复添加
+        if let existToastView = fromView.viewWithTag(tagForToastView) {
+            existToastView.removeFromSuperview()
+        }
+        
+        let containView = UIView()
+        containView.tag = tagForToastView
+        fromView.addSubview(containView)
+        containView.csFullfill()
+        
+        let shadowBg = UIView()
+        containView.addSubview(shadowBg)
+        shadowBg.backgroundColor = .black
+        shadowBg.alpha = 0.7
+        shadowBg.corner(radius: 5)
+        shadowBg.csCenter()
+        NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "H:|->=20-[v1]->=20-|", options: [], metrics: nil, views: ["v1":shadowBg]))
+        
+        let toastLabel = UILabel().wTextColor(.white).wFont(pfr(18)).wText(toast)
+        shadowBg.addSubview(toastLabel)
+        toastLabel.numberOfLines = 0
+        toastLabel.lineBreakMode = .byCharWrapping
+        toastLabel.csFullfill(top: 10, left: 10, bottom: -10, right: -10)
+        
+        let walltime = DispatchWallTime.now() + duration
+        DispatchQueue.main.asyncAfter(wallDeadline: walltime) {
+            UIView.animate(withDuration: 0.5, animations: {
+                containView.alpha = 0
+            }) { (finish) in
+                containView.removeFromSuperview()
+            }
         }
     }
 }
@@ -69,36 +71,38 @@ public func showLoading(loadingText:String?="加载中...",fromView: UIView?=nil
         return
     }
     
-    //防止重复添加
-    if let existLoadingView = fromView.viewWithTag(tagForLoadingView) {
-        existLoadingView.removeFromSuperview()
+    DispatchQueue.main.async {
+        //防止重复添加
+        if let existLoadingView = fromView.viewWithTag(tagForLoadingView) {
+            existLoadingView.removeFromSuperview()
+        }
+        
+        let containView = UIView()
+        containView.tag = tagForLoadingView
+        fromView.addSubview(containView)
+        containView.csFullfill()
+        
+        let shadowBg = UIView()
+        containView.addSubview(shadowBg)
+        shadowBg.backgroundColor = .black
+        shadowBg.alpha = 0.7
+        shadowBg.corner(radius: 11)
+        shadowBg.csCenter()
+        NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "H:|->=20-[v1]->=20-|", options: [], metrics: nil, views: ["v1":shadowBg]))
+        
+        let indicator = UIActivityIndicatorView()
+        indicator.style = .whiteLarge
+        shadowBg.addSubview(indicator)
+        indicator.startAnimating()
+        indicator.color = .white
+        indicator.csTop(20).csCenterX().csWidthHeight(37)
+        
+        let loadingLabel = UILabel().wTextColor(.white).wFont(pfr(18)).wText(loadingText)
+        shadowBg.addSubview(loadingLabel)
+        loadingLabel.numberOfLines = 0
+        loadingLabel.lineBreakMode = .byCharWrapping
+        loadingLabel.csCenterX().cstoBottomOf(view: indicator,constant: 10).csBottom(-20).csLeftRight(constant: 20)
     }
-    
-    let containView = UIView()
-    containView.tag = tagForLoadingView
-    fromView.addSubview(containView)
-    containView.csFullfill()
-    
-    let shadowBg = UIView()
-    containView.addSubview(shadowBg)
-    shadowBg.backgroundColor = .black
-    shadowBg.alpha = 0.7
-    shadowBg.corner(radius: 11)
-    shadowBg.csCenter()
-    NSLayoutConstraint.activate(NSLayoutConstraint.constraints(withVisualFormat: "H:|->=20-[v1]->=20-|", options: [], metrics: nil, views: ["v1":shadowBg]))
-    
-    let indicator = UIActivityIndicatorView()
-    indicator.style = .whiteLarge
-    shadowBg.addSubview(indicator)
-    indicator.startAnimating()
-    indicator.color = .white
-    indicator.csTop(20).csCenterX().csWidthHeight(37)
-    
-    let loadingLabel = UILabel().wTextColor(.white).wFont(pfr(18)).wText(loadingText)
-    shadowBg.addSubview(loadingLabel)
-    loadingLabel.numberOfLines = 0
-    loadingLabel.lineBreakMode = .byCharWrapping
-    loadingLabel.csCenterX().cstoBottomOf(view: indicator,constant: 10).csBottom(-20).csLeftRight(constant: 20)
 }
 
 public func hideLoading(from: UIView? = nil){
